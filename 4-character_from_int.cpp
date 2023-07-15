ff79ccd84674212bca6fbfd6dc389bcc7218e947
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2023-07-16 00:30:09 +0530

    Add files via upload

---
+++ 4-character_from_int.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    char c;
+    a=77;
+    c=a;
+    cout<<c<<endl;
+    c='p';
+    a=c;
+    cout<<a<<endl;
+    float f=1.9;
+    a=f;
+    cout<<a<<endl;
+}
