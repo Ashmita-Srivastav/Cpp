bf7e9ef91d36e5580c810a3de71f172dd57bdbb4
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2023-07-16 00:29:39 +0530

    Add files via upload

---
+++ 3-adding_2_no_userdefined.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the first number:";
+    cin >> a ;
+    cout<<"\n";
+    cout<<"Enter the second number:";
+    cin >> b; 
+  
+   cout<<"The answer is:";
+   cout << (a+b);
+
+}
