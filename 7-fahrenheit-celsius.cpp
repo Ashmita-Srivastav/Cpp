d71343a557b53ae59609690929bff64b6c29fb38
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2023-07-17 21:04:29 +0530

    Add files via upload

---
+++ 7-fahrenheit-celsius.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float f;
+    cout<<"Enter the temperature in fahrenheit:";
+    cin>>f;
+    float C= (f-32)*5/9;
+    cout<<C<<endl;
+} 
