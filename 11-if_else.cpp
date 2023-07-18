dbc86a52b51b82e950ba70c662b357887e55709c
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2023-07-18 21:34:30 +0530

    Add files via upload

---
+++ 11-if_else.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=10, b=20, c=100;
+    if(a<=b || c<=b){
+        cout<<"Hello"<<endl;
+    } else if (a<=b || a<=c){
+        cout<<"hi"<<endl;
+    } else{
+        cout<<"hey"<<endl;
+    }
+}
