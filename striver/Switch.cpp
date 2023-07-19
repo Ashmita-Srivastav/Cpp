fe6d5db03f27425d1e98cda75e902f86f0e2ac61
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2023-07-20 00:00:09 +0530

    Add files via upload

---
+++ striver/Switch.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int day;
+    cin >> day;
+    switch (day){
+        case 1:
+        cout<<"Monday";
+        case 2:
+        cout<<"Monday";
+        case 3:
+        cout<<"Monday";
+        case 4:
+        cout<<"Monday";
+        case 5:
+        cout<<"Monday";
+        case 6:
+        cout<<"Monday";
+        case 7:
+        cout<<"Monday";        
+
+    }
+}
