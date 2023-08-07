8e95e388f10e627144a855ac59c99845472c6c1e
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2023-08-07 22:50:32 +0530

    Add files via upload

---
+++ 33-Char_Pattern-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of rows:";
+    cin >> n;
+    int i=1;
+    
+    while(i<=n){
+        int j=1;
+        char s='A'+i-1+j-1;
+        while(j<=n){
+            cout<<s;
+            s=s+1;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+}
