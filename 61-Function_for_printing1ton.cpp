724cab8b95d891225036bb36ffe50eab2c2defa8
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2023-09-09 23:54:20 +0530

    Add files via upload

---
+++ 61-Function_for_printing1ton.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void print_1_to_n(int n){
+    int i=1;
+    while(i<=n){
+        cout<<i<<endl;
+        i++;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    print_1_to_n(n);
+    cout<<print_1_to_n;
+
+}
