1f8fa24ba489f9f0a866acef755f2c72841b809f
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2023-09-23 20:37:11 +0530

    Add files via upload

---
+++ 79-entering_elements_in_Array_directly.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void printArray(int input[],int n){
+    for(int i=0; i<n;i++){
+        cout<<input[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int input[100]={1,2,3};
+    printArray(input,100);
+    
+}
