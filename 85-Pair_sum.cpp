049645d08e52d8d0ac678ccb61b4b206dfd429be
Ashmita-Srivastav <138620566+Ashmita-Srivastav@users.noreply.github.com>
2024-01-12 20:55:16 +0530

    Add files via upload

---
+++ 85-Pair_sum.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int count=0;
+    int n;
+    cout<<"Enter the no. of elements:";
+    cin>>n;
+    int array[n];
+    for(int i=0; i<n ; i++){
+        cout<<"ENter the element:";
+        cin>>array[i];
+    }
+    int s;
+    cout<<"Enter the sum:";   
+    cin>>s;
+    
+    for(int i=0; i<n;i++){
+        for(int j=i+1; j<n;j++)
+        {
+            int sum=array[i]+array[j];
+            if(s==sum)
+            {
+                count=count+1;
+            }
+        }
+    }
+    cout<<count;
+}
